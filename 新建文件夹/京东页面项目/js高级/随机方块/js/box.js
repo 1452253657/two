e1bab8a478841173b47605af2d7cf67042e0c5ad
zhi <1452253657@qq.com>
2019-12-05 22:11:58 +0800

    123

--- 新建文件夹/京东页面项目/js高级/随机方块/js/box.js
+++ 新建文件夹/京东页面项目/js高级/随机方块/js/box.js
@@ -2,15 +2,15 @@ function Box(parent, options) {
     options = options || {};
     //设置对象的属性
     this.backgroundColor = options.backgroundColor || 'red'
-    this.width = options.width || 20 
-    this.height = options.height || 20 
+    this.width = options.width || 20
+    this.height = options.height || 20
     this.x = options.x || 0
     this.y = options.y || 0
 
     //创建相应的div
     this.div = document.createElement('div')
     parent.appendChild(this.div)
-    this.parent = parent 
+    this.parent = parent
 
     //设置div样式
     this.init()
@@ -32,14 +32,14 @@ Box.prototype.init = function () {
 //var box1 = new Box(parent)
 
 //随机生成方块的位置    random:随机的
-Box.prototype.random = function(){
-                             //父容器的宽度 /方块的宽度       =     总共能放多少个方块
-                             // 第几个数的位置*宽度 =横坐标
-    var x = Tools.getRandom(0,this.parent.offsetWidth/this.width-1)*this.width
-    var y = Tools.getRandom(0,this.parent.offsetHeight/this.height-1)*this.height
+Box.prototype.random = function () {
+    //父容器的宽度 /方块的宽度       =     总共能放多少个方块
+    // 第几个数的位置*宽度 =横坐标
+    var x = Tools.getRandom(0, this.parent.offsetWidth / this.width - 1) * this.width
+    var y = Tools.getRandom(0, this.parent.offsetHeight / this.height - 1) * this.height
 
-        this.div.style.left = x+'px'
-        this.div.style.top = y+'px'
+    this.div.style.left = x + 'px'
+    this.div.style.top = y + 'px'
 }
 
- 
+
